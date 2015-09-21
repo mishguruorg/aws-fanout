a847a99c2c56f6cf2652fa9c00cccfe8515fb50a
Jacob Duval <jladuval@gmail.com>
2015-09-21 16:29:07 +1200

    using obviously fake credentials

--- specs/helpers/getCredentials.js
+++ specs/helpers/getCredentials.js
@@ -1,8 +1,13 @@
 const getCredentials = () => {
+  // return {
+  //   region: 'us-west-2',
+  //   accessKeyId: 'AKIAIPYGFNCRIRVAXIAA',
+  //   secretAccessKey: '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'
+  // }
   return {
-    region: 'us-west-2',
-    accessKeyId: 'AKIAIPYGFNCRIRVAXIAA',
-    secretAccessKey: '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'
+    region: 'fpdosf2',
+    accessKeyId: 'fooA',
+    secretAccessKey: 'bar'
   }
 }
 
