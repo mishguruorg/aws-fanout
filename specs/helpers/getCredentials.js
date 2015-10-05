bb192aa95fe59c361ad43a3df835ff6941e31651
Jacob Duval <jladuval@gmail.com>
2015-10-06 11:38:34 +1300

    removed some logs. fixed up credentials

--- specs/helpers/getCredentials.js
+++ specs/helpers/getCredentials.js
@@ -1,14 +1,9 @@
 const getCredentials = () => {
   return {
-    region: 'us-west-2',
-    accessKeyId: 'AKIAIPYGFNCRIRVAXIAA',
-    secretAccessKey: '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'
+    region: 'fpdosf2',
+    accessKeyId: 'fooA',
+    secretAccessKey: 'bar'
   }
-  // return {
-  //   region: 'fpdosf2',
-  //   accessKeyId: 'fooA',
-  //   secretAccessKey: 'bar'
-  // }
 }
 
 export default getCredentials
