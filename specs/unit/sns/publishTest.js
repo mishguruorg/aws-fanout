a07bdba54dcad69ad63fa07d342a0574bc1a4c8b
Jacob Duval <jladuval@gmail.com>
2015-10-05 19:56:41 +1300

    added publish tests

---
+++ specs/unit/sns/publishTest.js
@@ -0,0 +1,46 @@
+/* globals beforeEach, afterEach, describe, it */
+import { expect } from 'chai'
+import { publish } from '../../../lib/'
+import AWS from 'aws-sdk'
+import sinon from 'sinon'
+import getCredentials from '../../helpers/getCredentials'
+import publishRes from '../../responses/publish'
+import createTopicResult from '../../responses/createTopic'
+
+describe('When I publish to an SNS Topic', () => {
+  beforeEach(() => {
+    mockPublish(publishRes, createTopicResult)
+  })
+
+  it('I should push an item into the topic', function (done) {
+    this.timeout(8000)
+
+    publish(getCredentials(), 'thisIsATestTopic', {
+      default: 'I am a default message'
+    })
+      .then(res => {
+        expect(res.MessageId).to.be.eq(publishRes.MessageId)
+        done()
+      })
+      .catch(done)
+  })
+
+  afterEach(() => {
+    restorePublish()
+  })
+})
+
+const mockPublish = (pubres, createTopicRes) => {
+  sinon.stub(AWS, 'SNS', () => ({
+    publish: (config, cb) => {
+      cb(null, pubres)
+    },
+    createTopic: (config, cb) => {
+      cb(null, createTopicRes)
+    }
+  }))
+}
+
+const restorePublish = () => {
+  AWS.SNS.restore()
+}
